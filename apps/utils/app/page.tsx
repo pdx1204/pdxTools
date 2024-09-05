fed6141bf32cbf2c14ebc6342e4c186f3a2602e6
pdx <qiuqfang98@qq.com>
2024-09-05 12:01:57 +0800

    refactor: update page component and add header component
    Refactor the `page.tsx` component to include a new `Header` component. This change improves the organization and modularity of the code. Additionally, the `Header` component is added to the `Home` component to display a list of tabs.
    
    Recent repository commits:
    - chore: add utils app
    - chore: update package name to "pdx-tools"
    - Update dependencies in package.json files
    - fix(deps): update nextjs monorepo to v13.5.5 (#19)
    - fix(deps): update dependency eslint-plugin-react to v7.33.2 (#28)
    - fix(deps): update dependency @types/node to v20.8.6 (#21)
    - chore: update
    - refactor: update
    - chore: update
    - chore: 抽离公共方法

--- apps/utils/app/page.tsx
+++ apps/utils/app/page.tsx
@@ -1,101 +1,114 @@
-import Image from "next/image";
+"use client";
+import { ChangeEventHandler, useState } from "react";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <div>
+      {/* <Header /> */}
+      <CommaSeparatedLinkString />
     </div>
   );
 }
+
+const TAB_LIST = [
+  {
+    name: "图片预览",
+  },
+  {
+    name: "URL Decode&Encode",
+  },
+  {
+    name: "颜色值转换",
+  },
+  {
+    name: "常用颜色",
+  },
+  {
+    name: "逗号分隔链接字符串",
+  },
+  {
+    name: "正则表达式提取文本",
+  },
+];
+const Header: React.FC<any> = () => {
+  return (
+    <div>
+      <ul className={"flex"}>
+        {TAB_LIST.map((item) => (
+          <li key={item.name} className={"mr-4 cursor-pointer"}>
+            {item.name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+const CommaSeparatedLinkString = () => {
+  const [needChangeValue, setNeedChangeValue] = useState("");
+  const [changeValue, setChangeValue] = useState("");
+  const [splitValue, setSplitValue] = useState(",");
+
+  const onNeedChangeValue: ChangeEventHandler<HTMLTextAreaElement> = (e) => {
+    const value = e.target.value;
+    setNeedChangeValue(value);
+    console.log(value, "fsdfsa", value.split("\n"));
+    setChangeValue(value.split("\n").join(splitValue));
+  };
+
+  const optionList = [
+    {
+      name: "加单引号",
+    },
+    {
+      name: "加双引号",
+    },
+    {
+      name: "去除第一个",
+    },
+    {
+      name: "每行一个",
+    },
+    {
+      name: "去除引号",
+    },
+    {
+      name: "复制",
+    },
+    {
+      name: "清空",
+    },
+  ];
+  return (
+    <div className={" text-xs px-5 py-5"}>
+      <textarea
+        className={"w-full border border-red-400 rounded p-2 text-base"}
+        rows={10}
+        name=""
+        id=""
+        value={needChangeValue}
+        onChange={onNeedChangeValue}
+      ></textarea>
+      <div className={"my-5"}>
+        <input
+          type="text"
+          className={"w-[50px] px-1 py-1 border border-red-400 rounded"}
+          value={splitValue}
+          onChange={(e) => setSplitValue(e.target.value)}
+        />
+        {optionList.map((item) => (
+          <button key={item.name} className={"mx-2 text-base"}>
+            {item.name}
+          </button>
+        ))}
+      </div>
+      <textarea
+        className={"w-full border border-red-400 rounded p-2 text-base"}
+        rows={10}
+        name=""
+        id=""
+        value={changeValue}
+      ></textarea>
+    </div>
+  );
+};
